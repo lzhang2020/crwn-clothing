77c4ac26434454c0e09bba6cf6a69905882dcf4d
lzhang2020 <amandaaabbb@yahoo.com>
2021-08-13 22:42:30 -0500

    updating our application with reselect, replaced cart selectors with memoized selectors

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -2,24 +2,34 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
-
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
 import './cart-icon.styles.scss';
 
-const CartIcon = ({ toggleCartHidden }) => (
-    <div className='cart-icon' onClick={toggleCartHidden}>
-        <ShoppingIcon className='shopping-icon' />
-        <span className='item-count'>0</span>
-    </div>
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
+	<div className='cart-icon' onClick={toggleCartHidden}>
+		<ShoppingIcon className='shopping-icon' />
+		<span className='item-count'>{itemCount}</span>
+	</div>
 );
 
 const mapDispatchToProps = dispatch => ({
-    toggleCartHidden: () => dispatch(toggleCartHidden())
+	toggleCartHidden: () => dispatch(toggleCartHidden())
 })
 
+// const mapStateToProps = ({ cart: { cartItems } }) => ({
+// 	itemCount: cartItems.reduce((accumalatedQuantity, cartItem) => accumalatedQuantity + cartItem.quantity, 0)
+// })
+
+const mapStateToProps = (state) => ({
+	itemCount: selectCartItemsCount(state)
+})
+
+
+
 export default connect(
-    null, 
-    mapDispatchToProps
-    )(CartIcon);
+	mapStateToProps,
+	mapDispatchToProps
+)(CartIcon);
