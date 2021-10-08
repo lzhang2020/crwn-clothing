4512ec5d8bf0bbd60a693a3052abfc725a85f857
lzhang2020 <amandaaabbb@yahoo.com>
2021-10-08 11:36:28 -0500

    move our directory and shop data into its own reducer, created respective selectors and updated corresponding components with ux flow for directory/shop

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -9,9 +9,17 @@ import './collection.styles.scss';
 
 const CollectionPage = ({ collection}) => {
     console.log("check collection", collection);
+
+    const { title, items } = collection;
     return (
         <div className='collection-page'>
-            <h2>Collection PAGE</h2>
+            <h2 className='title'>{title}</h2>
+            <div className='items'>
+                {items.map(item => (
+                    <CollectionItem key={item.id} item={item}/>
+                ))}
+
+            </div>
         </div>
     );
 };
